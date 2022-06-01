9a0310b07e181ee89ee1995dabddfd69abab8713
sonnv0812phutq194138 <phu.tq194138@sis.hust.edu.vn>
2022-06-01 13:48:40 +0700

    try

---
+++ backtracking.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 200;
+
+int n, k;
+int d_min = 2147483647; //Quang duong nho nhat
+int d = 0; //tong quang duong hien tai
+int p = 0; //so hanh khach ten xe
+int c[N][N]; //ma tran khoang cach
+
+
+int main() {
+	
+	return 0;
+}
